5e45a08c501f1abc0b38c2435434f0369e88c369
Marcell <mpather47@gmail.com>
2020-06-21 11:32:57 +0200

    first commit

---
+++ MatchingGame/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace MatchingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1390, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1557, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1390, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Time left:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1557, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "30";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Level 1",
+            "Level 2",
+            "Level 3",
+            "Level 4",
+            "Level 5"});
+            this.comboBox1.Location = new System.Drawing.Point(1397, 217);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(196, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1397, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1798, 965);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fruity Match";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+    }
+}
+
---
+++ MatchingGame/Form1.cs
@@ -0,0 +1,454 @@
+﻿/*MatchingGame
+ * Author: Marcell Pather 2A (217053947)
+ * Date: 20 March 2019
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            //This code runs the second Form first
+            Thread t = new Thread(new ThreadStart(MatchingGame));
+            
+            t.Start();
+            Thread.Sleep(5000);
+            InitializeComponent();
+            t.Abort();
+            
+        }
+        public void MatchingGame()
+        {
+            
+            Form2 frm2 = new Form2();
+            frm2.ShowDialog();
+           
+        }
+        
+        List<Button> buttons = new List<Button>();
+        String[] arr = new String[] { "Card1.png", "Card1.png", "Card2.png", "Card2.png", "Card3.png",
+                "Card3.png", "Card4.png", "Card4.png","Card5.png", "Card5.png", "Card6.png", "Card6.png",
+                "Card7.png", "Card7.png", "Card8.png", "Card8.png" , "Card9.png", "Card9.png", "Card10.png",
+                "Card10.png", "Card11.png", "Card11.png", "Card12.png", "Card12.png" ,
+                "Card13.png", "Card13.png", "Card14.png", "Card14.png", "Card15.png",
+                "Card15.png", "Card16.png", "Card16.png", "Card17.png", "Card17.png" ,
+                "Card18.png", "Card18.png", "Card19.png", "Card19.png", "Card20.png"  ,
+                "Card20.png", "Card11.png", "Card11.png", "Card12.png", "Card12.png" ,
+                "Card13.png", "Card13.png", "Card14.png", "Card14.png", "Card15.png",
+                "Card15.png", "Card16.png", "Card16.png", "Card17.png", "Card17.png"  ,
+                "Card18.png", "Card18.png", "Card19.png", "Card19.png", "Card20.png",
+                "Card20.png", "Card4.png", "Card4.png","Card5.png", "Card5.png"};
+        int n = 12;
+        PictureBox[] Shapes ;
+        PictureBox pendingImage1;
+        PictureBox pendingImage2;
+        int[] Tag = new int[] { 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,
+                                9,9,10,10,11,11,12,12,13,13,14,14,
+                                15,15,16,16,17,17,18,18,19,19,20,20,11,11,
+                                12,12,13,13,14,14,15,15,16,16,17,17,
+                                18,18,19,19,20,20,4,4,5,5};
+        Random random1 = new Random();
+        Panel dynamicPanel = new Panel();
+        int y = 0;
+        int x = 0;
+        int counter = 0;
+        int score = 0;
+        string text = "Level 1";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dynamicPanel.BackColor = Color.Yellow;
+        
+            timer1.Start();
+            Controls.Add(dynamicPanel);
+            Shapes = new PictureBox[n];
+            //this loop srambles up the picturs
+            for (int i = 0; i < Shapes.Length; i++)
+            {
+                
+                random(i);
+               
+            }
+            //this loop displays the scrambled up pictures
+            for (int i = 0; i < Shapes.Length; i++)
+            {
+                display(i);
+            }
+           
+            //resizes image to apporopriate size 
+            foreach (PictureBox picture in dynamicPanel.Controls)
+            {
+                picture.SizeMode = PictureBoxSizeMode.StretchImage;
+                picture.Cursor = Cursors.Hand;
+            }
+        }
+        /*This method adds the pictur files into an array and formats the images so it can
+        be displayed in formated rows and columns*/
+        #region Display
+        public void display(int i)
+        {
+
+            Shapes[i] = new PictureBox();
+            Shapes[i].Name = "Picture" + i.ToString();
+            Shapes[i].Size = new Size(100, 120);
+           
+            Shapes[i].Image = Image.FromFile(Application.StartupPath + "\\Cards\\" + arr[i]);
+            Shapes[i].Visible = true;
+            Shapes[i].Tag = Tag[i];
+            Shapes[i].Enabled = false;
+
+            dynamicPanel.Controls.Add(Shapes[i]);
+            dynamicPanel.Size = new System.Drawing.Size(1900, 1000);
+            int n = i;
+            Shapes[i].Click += (object s, EventArgs ea) => ChangeImage(n);
+            x = x + 100;
+
+            if (i == 0)
+            {
+                x = 100;
+                y = 50;
+                if (Shapes.Length == 12)
+                {
+                    x = 400;
+                }else if (Shapes.Length == 24)
+                {
+                    x = 250;
+                }
+                else if (Shapes.Length == 32)
+                {
+                    x = 90;
+                }
+                else if (Shapes.Length == 48|| Shapes.Length == 64)
+                {
+                    x = 80;
+                    y = 30;
+                }
+                
+            }
+          
+
+            if (Shapes.Length == 12)
+            {
+                if (i == 3 || i == 6||i==9)
+                {
+                    y += 120;
+                    x = 400;
+
+                }
+
+            }
+
+            else if (Shapes.Length == 24)
+            {
+
+                if (i == 6 || i == 12 || i == 18)
+                {
+                    y += 120;
+                    x = 250;
+
+                }
+
+            }
+            else if (Shapes.Length == 32)
+            {
+
+                if (i == 8 || i == 16 || i == 24)
+                {
+                    y += 120;
+                    x = 90;
+
+                }
+
+            }
+            else if (Shapes.Length == 48)
+            {
+
+                if (i == 8 || i == 16 || i == 24||i==32||i==40)
+                {
+                    y += 100;
+                    x = 80;
+                    
+                }
+
+            }
+            else if (Shapes.Length == 64)
+            {
+
+                if (i == 8 || i == 16 || i == 24 || i == 32 || i == 40||i==48|i==56)
+                {
+                   
+                    y += 90;
+                    x = 80;
+              
+                }
+
+            }
+            Shapes[i].Location = new Point(x, y);
+
+           
+            
+        }
+        #endregion
+        /*This method uses a random sorting algorithm which scrambles up the images in the 
+         * array
+         */
+        #region Random Pictures
+        public void random(int i)
+        {
+            int k = random1.Next(Shapes.Length);
+            String value = arr[k];
+            arr[k] = arr[i];
+            arr[i] = value;
+
+            int value2 = Tag[k];
+            Tag[k] = Tag[i];
+            Tag[i] = value2;
+        }
+        #endregion
+        /*This method checks if you click on a picture and matches images of the same type.
+         * It keeps track of the amount of mistakes you do.
+         */
+        #region Change Images
+        void ChangeImage(int index)
+        {
+            
+            timer3.Start();
+           
+            // Process the panel clicks here
+            Shapes[index].Image = Image.FromFile(Application.StartupPath+"\\Cards\\" + arr[index]);
+            if (pendingImage1==null)
+            {
+                pendingImage1 = Shapes[index];
+               pendingImage1.Enabled = false;
+
+            }else if (pendingImage1 !=null && pendingImage2 ==null)
+            {
+                pendingImage2 = Shapes[index];
+                pendingImage2.Enabled = false;
+            }
+
+            if (pendingImage1 != null && pendingImage2 != null)
+            {
+                if ((int)pendingImage1.Tag == (int)pendingImage2.Tag)
+                {
+                    pendingImage1 = null;
+                    pendingImage2 = null;
+                    counter++;
+                    
+                }
+                else
+                {
+                    timer2.Start();
+                    score += 10;
+                    label2.Text = Convert.ToString(score);
+                    pendingImage1.Enabled = true;
+                    pendingImage2.Enabled = true;
+                }
+            }
+             
+           
+        }
+        #endregion
+       
+        #region Timers
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            timer1.Stop();
+           
+            //if three seconds has passed it hides the pictures
+            for (int i = 0; i < Shapes.Length; i++)
+            {
+                Shapes[i].Enabled = true;
+                Shapes[i].Image = Image.FromFile(Application.StartupPath+ "\\Cards\\Cover.png");
+
+            }
+        }
+        //if the two pictures clicked doesnt match it hides the pictures
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            pendingImage1.Image = Image.FromFile(Application.StartupPath + "\\Cards\\Cover.png");
+            pendingImage2.Image = Image.FromFile(Application.StartupPath + "\\Cards\\Cover.png");
+            pendingImage1 = null;
+            pendingImage2 = null;
+        }
+        /*this method keeps track of the time in the game and also checks if 
+          all the images are matched to stop the time from incrementing
+          */
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int timer = Convert.ToInt32(label4.Text);
+            timer = timer -1;
+            label4.Text = Convert.ToString(timer);
+            if (counter==(n/2) )
+            {
+                timer3.Stop();
+               
+                MessageBox.Show("You Win!");
+                
+                foreach (PictureBox picture in dynamicPanel.Controls)
+                {
+                    picture.Enabled = false;
+                }
+                
+            }
+            else
+            if (timer ==0)
+            {
+                timer3.Stop();
+                MessageBox.Show("You Lose!");
+                foreach (PictureBox picture in dynamicPanel.Controls)
+                {
+                    picture.Enabled = false;
+                }
+                
+               
+            }
+           
+        }
+        #endregion
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            counter = 0;
+            for (int j = 0; j < Shapes.Length; j++)
+            {
+                Shapes[j].Image = null;
+                dynamicPanel.Controls.Remove(Shapes[j]);
+            }
+            Controls.Add(dynamicPanel);
+            text = comboBox1.SelectedItem.ToString();
+
+            switch (text)
+            {
+                case "Level 1":
+                    timer3.Stop();
+                    score = 0;
+                    label4.Text = Convert.ToString("30");
+                    label2.Text = Convert.ToString("0");
+                    n = 12;
+                    Shapes = new PictureBox[n];
+                    
+                    timer1.Start();
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+                        random(i);
+
+                    }
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+
+                        display(i);
+                    }
+                    break;
+                case "Level 2":
+                    timer3.Stop();
+                    score = 0;
+                    label4.Text = Convert.ToString("40");
+                    label2.Text = Convert.ToString("0");
+                    n = 24;
+                    Shapes = new PictureBox[n];
+                    timer1.Start();
+                    
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+                        random(i);
+                        
+                    }
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+                        
+                        display(i);
+                    }
+                    break;
+                case "Level 3":
+                    score = 0;
+                    timer3.Stop();
+                    label4.Text = Convert.ToString("50");
+                    label2.Text = Convert.ToString("0");
+                    n = 32;
+                    Shapes = new PictureBox[n];
+                    timer1.Start();
+
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+                        random(i);
+
+                    }
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+
+                        display(i);
+                    }
+                    break;
+                case "Level 4":
+                    score = 0;
+                    timer3.Stop();
+                    label4.Text = Convert.ToString("75");
+                    label2.Text = Convert.ToString("0");
+                    n = 48;
+                    Shapes = new PictureBox[n];
+                    timer1.Start();
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+                        random(i);
+
+                    }
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+
+                        display(i);
+                        Shapes[i].Size = new Size(80, 100);
+                    }
+                 
+                    break;
+                case "Level 5":
+                    score = 0;
+                    timer3.Stop();
+                    label4.Text = Convert.ToString("90");
+                    label2.Text = Convert.ToString("0");
+                    n = 64;
+                    Shapes = new PictureBox[n];
+                    timer1.Start();
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+                        random(i);
+
+                    }
+                    for (int i = 0; i < Shapes.Length; i++)
+                    {
+
+                        display(i);
+                        Shapes[i].Size = new Size(80, 80);
+                    }
+                   
+                    break;
+            }
+          
+
+
+            foreach (PictureBox picture in dynamicPanel.Controls)
+            {
+                picture.SizeMode = PictureBoxSizeMode.StretchImage;
+                picture.Cursor = Cursors.Hand;
+            }
+        }
+
+        
+    }
+        
+}
